e72a502b256dd4a159e38858fd0ac22c374503c2
selcukarabacak-lab <selcukarabacak@gmail.com>
2026-08-20 14:06:39 +0300

    Bump KARÇİMSA Android to v1.1.3

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.karcimsa.mobile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.1.2"
+        versionCode = 6
+        versionName = "1.1.3"
     }
 
     buildTypes {
