bfa5c224d595cd62d01a339a07b7551b4bf3e355
selcukarabacak-lab <selcukarabacak@gmail.com>
2026-08-22 15:34:31 +0300

    Bump Android app to v1.1.10 for launcher icon fix

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.karcimsa.mobile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 12
-        versionName = "1.1.9"
+        versionCode = 13
+        versionName = "1.1.10"
     }
 
     buildTypes {
