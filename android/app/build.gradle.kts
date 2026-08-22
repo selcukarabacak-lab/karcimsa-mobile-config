c8baa24814d971cc29188db7fcbe9bbe1f4e96a9
selcukarabacak-lab <selcukarabacak@gmail.com>
2026-08-22 14:40:05 +0300

    Enable BuildConfig generation for settings version display

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,7 +28,10 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions { jvmTarget = "17" }
-    buildFeatures { viewBinding = true }
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
+    }
 }
 
 dependencies {
