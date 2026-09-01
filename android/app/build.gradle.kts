68760385f30a3d129ad985b2ad0442985d02bb2d
selcukarabacak-lab <selcukarabacak@gmail.com>
2026-09-01 19:49:57 +0300

    Bump Android app to 1.1.9.3

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.karcimsa.mobile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 15
-        versionName = "1.1.9.2"
+        versionCode = 16
+        versionName = "1.1.9.3"
     }
 
     buildTypes {
